113c97833171021b78883b7f5a0a1a2ee6064c98
Christian Ruiz <christian25555@gmail.com>
2015-04-28 01:48:45 -0400

    Pitch (up/down) and yaw(left/right) updated for servos

--- RobOculus/RobOculus/Servos.cpp
+++ RobOculus/RobOculus/Servos.cpp
@@ -4,6 +4,7 @@
 #include <windows.h>
 #include <Extras\OVR_Math.h>
 #include <conio.h>
+#include <chrono>
 
 using namespace std;
 using namespace OVR;
@@ -22,7 +23,7 @@ OculusRift::OculusRift(){//initialize the SDK
 
 	//init other needed values
 	h = GetStdHandle(STD_OUTPUT_HANDLE);
-	SP = new Serial("COM5");
+	SP = new Serial("COM6");
 	//begin head tracking
 	// Optional: we can overwrite the previous console to more
 	// easily see changes in values
@@ -39,7 +40,6 @@ OculusRift::~OculusRift(){//a "destructor"
 }
 
 void OculusRift::trackHMD(){
-	//cout << "got in" << endl;
 	while (1){
 		ovrTrackingState ts = ovrHmd_GetTrackingState(hmd, frameTiming.ScanoutMidpointSeconds);
 
@@ -47,27 +47,38 @@ void OculusRift::trackHMD(){
 			// The cpp compatibility layer is used to convert ovrPosef to Posef (see OVR_Math.h)
 			Posef pose = ts.HeadPose.ThePose;
 			float yaw, pitch, roll;
+			ovrSensorData state = ts.RawSensorData;//use termpature for torque for the continous servo?
+			float accel = state.Accelerometer.y;
+			
 			pose.Rotation.GetEulerAngles<Axis_Y, Axis_X, Axis_Z>(&yaw, &pitch, &roll);
-
+			
 			// Optional: move cursor back to starting position and print values
 			//SetConsoleCursorPosition(h, bufferInfo.dwCursorPosition);
 
-			//if (SP->IsConnected()){
-			degree = (RadToDegree(yaw) - 90) * -1;
-			char *cstr = new char[2];
+			if (SP->IsConnected()){
+				yaw = (RadToDegree(yaw) - 90) * -1;
+				pitch = RadToDegree(pitch) + 90;
+		
+				if (accel > 0){
+					if (pitch > 90) pitch = 180;
+					else pitch = 0;
+				}
+				if (yaw < 0)yaw = 0;
+				else if (yaw > 180)yaw = 180;
 
-			if (degree < 0)degree = 0;
-			else if (degree > 180)degree = 180;
+				char *cstr = new char[2];
+				cstr[0] = (char)(yaw / 2);
+				cstr[1] = (char)(pitch / 2);
+				SP->WriteData(cstr, 2);
 
-			cstr[0] = (char)(degree / 2);
-			//cout << "\rDegree: " << degree << endl;
-			//SP->WriteData(cstr, 1);
+				//cout << state.Temperature << endl;
 
-			//}
+				cout << pitch << endl;				
+			}
 			if (_kbhit()) exit(0);
 		}
 
-		Sleep(150);
+		Sleep(50);
 	}
 
 }
