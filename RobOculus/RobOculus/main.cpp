851163b6263e4d0c891130b3f567f72a729aa9c5
Christian Ruiz <christian25555@gmail.com>
2015-05-05 15:11:57 -0400

    random

--- RobOculus/RobOculus/main.cpp
+++ RobOculus/RobOculus/main.cpp
@@ -7,11 +7,11 @@ int main(){
 	OculusRift *ovr = new OculusRift;
 	XboxController *xbox = new XboxController;
 
-	std::thread updateOVR(&OculusRift::trackHMD, ovr);
+	//std::thread updateOVR(&OculusRift::trackHMD, ovr);
 	std::thread updateTimer(&XboxController::updateTimer, xbox);//create a thread from a member function
 	std::thread updateController(&XboxController::getState, xbox, ovr);
 	
-	updateOVR.join();
+	//updateOVR.join();
 	updateTimer.join();
 	updateController.join();
 
